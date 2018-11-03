ac63def4728ab588f12903c0961b34ca32d4a2ab
Administrator <Administrator@JF005010>
2018-11-03 16:49:26 +0800

    提交

---
+++ src/lession5/Test1.java
@@ -0,0 +1,17 @@
+package lession5;
+
+public class Test1 {
+	public static void main(String[] args) {
+		Circle c1 = new Circle();
+		Circle c2 = new Circle();
+		c1.radius = 10;
+		System.out.println(c1.getArea());
+		System.out.println(c1.getPerimeter());
+		c2.radius = 20;
+		System.out.println(c2.getArea());
+		System.out.println(c2.getPerimeter());
+		c1.radius = 30;
+		System.out.println(c1.getArea());
+		System.out.println(c1.getPerimeter());
+	}
+}
